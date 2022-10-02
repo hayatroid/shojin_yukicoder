2d59396533f48db7300479f0bee69383305d90eb
hayatroid <hayatroid@gmail.com>
2022-10-03 02:04:55 +0900

    解いた！

--- 405/src/bin/c.rs
+++ 405/src/bin/c.rs
@@ -1,3 +1,15 @@
+fn read() -> i64 {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s.trim().parse().unwrap()
+}
+
 fn main() {
-    todo!();
+    let a = read();
+    let b = read();
+    if a == 1 && b == 0 {
+        println!("20221003"); // なんでもよい
+    } else {
+        println!("{}", b / (1 - a));
+    }
 }
