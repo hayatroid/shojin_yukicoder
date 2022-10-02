e0750d5885f53d7cbbbc888c1466cd7761212480
hayatroid <hayatroid@gmail.com>
2022-10-03 01:54:56 +0900

    解いた！

--- 405/src/bin/b.rs
+++ 405/src/bin/b.rs
@@ -1,3 +1,20 @@
+fn read() -> usize {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s.trim().parse().unwrap()
+}
+
 fn main() {
-    todo!();
+    let n = read();
+    let mut m = read();
+    let mut ans = String::new();
+    if m == 0 {
+        println!("0");
+        return;
+    }
+    while m > 0 {
+        ans = format!("{}{}", m % n, ans);
+        m /= n;
+    }
+    println!("{}", ans);
 }
